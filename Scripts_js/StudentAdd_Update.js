f6e77cf5bd1f4252f7b7dffd958100035d009c10
shivraj tiwaskar <shivrajtiwaskar7@gmail.com>
2023-10-12 22:07:26 +0530

    async await

--- Scripts_js/StudentAdd_Update.js
+++ Scripts_js/StudentAdd_Update.js
@@ -36,28 +36,56 @@ const btn_get_data = document.getElementById('btn_get_data')
 btn_get_data.addEventListener("click", getDataHandlear)
 
 function getDataHandlear() {
+    debugger
+    console.log('start.........')
     const id = document.getElementById('get_id').value;
     const params = {
         student_id: parseInt(id)
     }
- 
+    let used_data_after_fetch = '';
 
-    fetch("/Student/get_data", {
+    //fetch("/Student/get_data", {
+    //    method: 'POST',
+    //    headers: {
+    //        'Content-Type': 'application/json',
+    //        'Accept': 'application/json',
+    //    },
+    //    body: JSON.stringify(params)
+    //}).then((resp) => resp.json())
+    //    .then((resp1) => JSON.parse(resp1))
+    //    .then((final_result) => {
+    //        console.log('wating.........')
+    //        FirstName.value = final_result.Table[0].firstName
+    //        LastName.value = final_result.Table[0].lastName
+    //        Contact.value = final_result.Table[0].contact
+    //        dateOfBirth.value = final_result.Table[0].DOB
+    //        used_data_after_fetch = final_result;
+    //    })
+    getDataFromServer("/Student/get_data", params)
+        .then((result) => {
+            FirstName.value = result.Table[0].firstName
+            LastName.value = result.Table[0].lastName
+            Contact.value = result.Table[0].contact
+            dateOfBirth.value = result.Table[0].DOB
+        })
+
+
+
+    console.log(used_data_after_fetch, params)
+    console.log('END.........')
+}
+
+
+async function getDataFromServer(url, params) {
+
+    return await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
         },
         body: JSON.stringify(params)
-    }).then((resp) => resp.json())
+    })
+        .then((resp) => resp.json())
         .then((resp1) => JSON.parse(resp1))
-        .then((final_result) => {
-            FirstName.value = final_result.Table[0].firstName
-            LastName.value = final_result.Table[0].lastName
-            Contact.value = final_result.Table[0].contact
-            dateOfBirth.value = final_result.Table[0].DOB
-        })
-
 }
-
-
